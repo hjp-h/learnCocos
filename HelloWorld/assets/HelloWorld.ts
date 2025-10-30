ac38b5af57f8986b817aa2bf4bd0c73030f23ac1
hongjp <hongjp@flatincbr.com>
2025-10-30 11:58:04 +0800

    feat: 文件初始化

---
+++ HelloWorld/assets/HelloWorld.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("HelloWorld")
+export class HelloWorld extends Component {
+  start() {
+    console.log("Hello World!");
+  }
+
+  update(deltaTime: number) {}
+}
